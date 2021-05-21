a9945f77f38af819cab571e896324072478d26ca
Poom Yimyuean <lebrancconvas@gmail.com>
2021-05-21 11:11:50 +0700

    :art: Edit Copyright Style.

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -7,14 +7,13 @@ footer {
   position: absolute;
   bottom: 0;
   width: 100%;
-  height: 5px;
+  height: 3px;
   padding-top: 10px;
   align-items: center;
   justify-content: center;
   text-align: center;
   background: black;
   color: white;
-  padding-top: 10px;
   padding-bottom: 5.4%;
 }
 </style>
