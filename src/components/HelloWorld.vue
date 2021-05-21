88218bdc2e933d9d5e217cc642caa1144dcd057e
Poom Yimyuean <lebrancconvas@gmail.com>
2021-05-21 14:17:49 +0700

    :fire: Delete Style.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,6 @@
 <template>
-  <div>Welcome to Co Space</div>
+  <div class="h1">Welcome to Co Space</div>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
