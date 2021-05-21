87729ac9efe48cfcfd13150afa856b7d7e8610d4
Poom Yimyuean <lebrancconvas@gmail.com>
2021-05-21 14:33:22 +0700

    :sparkles: Add Sign Path.

---
+++ src/components/Sign.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button type="button" id="signup" class="btn btn-secondary">Sign Up</button>
+    <button type="button" id="login" class="btn btn-primary">Log in</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div #signup {
+  margin: 0px 10px 0px 0px;
+}
+
+div #login {
+  margin: 0px 10px 0px 0px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/desk.png" />
     <HelloWorld />
+    <Sign />
   </div>
 </template>
 
@@ -9,10 +10,12 @@
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
+import Sign from "@/components/Sign.vue";
 @Options({
   components: {
     HelloWorld,
     Navbar,
+    Sign,
   },
 })
 export default class Home extends Vue {}
