fd807e6e1ece4c26ab597ff3d18955dc62907955
Poom Yimyuean <lebrancconvas@gmail.com>
2021-05-21 11:07:50 +0700

    :art: Add Footer Style.

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -2,4 +2,19 @@
   <footer>Copyright © 2021 Co-Space. All Rights Reserved.</footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 5px;
+  padding-top: 10px;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background: black;
+  color: white;
+  padding-top: 10px;
+  padding-bottom: 5.4%;
+}
+</style>
