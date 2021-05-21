f4b23d4e12408756b9637710f63e36ff9e44885e
Poom Yimyuean <lebrancconvas@gmail.com>
2021-05-21 23:34:15 +0700

    :sparkles: Add Signup Form.

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,5 +1,46 @@
 <template>
-  <div>Sign Up Page.</div>
+  <div>
+    <p class="text-center h2">Login</p>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">Username</span>
+      </div>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Enter Your Username"
+        aria-label="Username"
+        aria-describedby="basic-addon1"
+      />
+    </div>
+
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">Password</span>
+      </div>
+      <input
+        type="password"
+        class="form-control"
+        placeholder="Enter Your Password"
+        aria-label="Password"
+        aria-describedby="basic-addon1"
+      />
+    </div>
+
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">Confirm Password</span>
+      </div>
+      <input
+        type="password"
+        class="form-control"
+        placeholder="Confirm Your Password"
+        aria-label="Password"
+        aria-describedby="basic-addon1"
+      />
+    </div>
+    <button type="button" class="btn btn-warning">Sign Up</button>
+  </div>
 </template>
 
 <script>
