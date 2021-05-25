bc167e55e7d2ae8cd9844800946e73a958702f07
Poom Yimyuean <lebrancconvas@gmail.com>
2021-05-25 17:56:08 +0700

    :sparkles: Connect Login to Database.

--- server.js
+++ server.js
@@ -1,12 +1,60 @@
 /* eslint-disable */
 const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'cospace'
+})
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.use(bodyParser.urlencoded({ extented: true }))
+app.use(bodyParser.json())
 
 app.use(express.static(__dirname + "/dist/"));
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(__dirname + "/dist/index.html");
+    res.sendFile(__dirname + "/dist/index.html");
 });
+
+app.post('/auth', (req, res) => {
+    let username = req.body.username
+    let password = req.body.password
+    if (username && password) {
+        connection.query('SELECT * FROM account WHERE username = ? AND password = ?', [username, password], (error, results, fields) => {
+            if (results.length > 0) {
+                req.session.loggedin = true
+                req.session.username = username
+                res.redirect('/home')
+            } else {
+                res.send('Wrong Username and/or Password.')
+            }
+            res.end()
+        })
+    } else {
+        res.send('Please Enter Username and Password !!')
+        res.end()
+    }
+})
+
+app.get('/home', (req, res) => {
+    if (req.session.loggedin) {
+        res.send(`Welcome ${req.session.username}`)
+    } else {
+        res.send('Please Login to be in This Page.')
+    }
+    res.end()
+})
 
 app.listen(port);
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,32 +1,34 @@
 <template>
   <div>
     <p class="text-center h2">Login</p>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Username</span>
+    <form action="/auth" method="POST">
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">Username</span>
+        </div>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Enter Your Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
       </div>
-      <input
-        type="text"
-        class="form-control"
-        placeholder="Enter Your Username"
-        aria-label="Username"
-        aria-describedby="basic-addon1"
-      />
-    </div>
 
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Password</span>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">Password</span>
+        </div>
+        <input
+          type="password"
+          class="form-control"
+          placeholder="Enter Your Password"
+          aria-label="Password"
+          aria-describedby="basic-addon1"
+        />
       </div>
-      <input
-        type="password"
-        class="form-control"
-        placeholder="Enter Your Password"
-        aria-label="Password"
-        aria-describedby="basic-addon1"
-      />
-    </div>
-    <button type="button" class="btn btn-primary">Login</button>
+      <input type="submit" class="btn btn-primary" value="Submit" />
+    </form>
   </div>
 </template>
 
